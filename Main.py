db0774fa32b6fc5854adde132ca843396a9af37b
Liko <150863666+Liko0o0@users.noreply.github.com>
2023-11-25 20:22:46 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,3 @@
-# Tout traduire en anglais
-
 # [Command to create .EXE with pyinstaller]
 # pyinstaller Main.py --onefile --noconsole --add-data "Sound/music_main.mp3;Sound" --add-data "Sound/music_game.mp3;Sound" --add-data "Sound/button_click.mp3;Sound" --add-data "Sound/cell_click.mp3;Sound" --add-data "Sound/easter_egg.mp3;Sound" --add-data "Sound/explosion.mp3;Sound" --add-data "Sound/cell_flag.mp3;Sound" --add-data "Sound/win.mp3;Sound" --add-data "Easy.py;." --add-data "Medium.py;." --add-data "Hard.py;." --add-data "Achievements.py;." --add-data "Time.py;." --add-data "Image/english.png;Image" --add-data "Image/french.png;Image" --add-data "Image/img_sound_mute.png;Image" --add-data "Image/img_sound.png;Image" --add-data "Image/icon.ico;Image" --icon=Image/icon.ico
 
